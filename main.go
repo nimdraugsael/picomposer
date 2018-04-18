b50783489e8ce36f9099a8fb6fbcaa257f2dc98f
Aleksei Shaikhaleev <nimdraug.sael@gmail.com>
2018-04-18 14:02:59 +0700

    refactor

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ type Picomposer struct {
 	assetsFolder string
 	outputFolder string
 	tuples [][]namedImage
-	images map[int][]namedImage
+	images [][]namedImage
 }
 
 type namedImage struct {
@@ -32,7 +32,7 @@ type namedImage struct {
 
 
 func (p *Picomposer) buildTuples(n int, res []namedImage) {
-	if p.images[n] == nil {
+	if len(p.images) == n {
 		p.tuples = append(p.tuples, res)
 		return
 	}
@@ -48,19 +48,25 @@ func (p *Picomposer) loadImages() {
 		log.Fatal(err)
 	}
 
-	for i, layerFolder := range files {
+	for _, layerFolder := range files {
 		if layerFolder.IsDir() {
+			imgs := make([]namedImage, 0)
 			imgFiles, err := ioutil.ReadDir(p.assetsFolder + "/" + layerFolder.Name())
 			if err != nil {
 				log.Fatal(err)
 			}
 			for _, f := range imgFiles {
-				img, err := gg.LoadPNG(p.assetsFolder + "/" + layerFolder.Name() + "/" + f.Name())
+				path := p.assetsFolder + "/" + layerFolder.Name() + "/" + f.Name()
+				img, err := gg.LoadPNG(path)
+
 				if err != nil {
 					log.Fatal(err)
 				}
 				name := strings.Split(f.Name(), ".")[0]
-				p.images[i] = append(p.images[i], namedImage{img, name})
+				imgs = append(imgs, namedImage{img, name})
+			}
+			if len(imgs) > 0 {
+				p.images = append(p.images, imgs)
 			}
 		}
 	}
@@ -74,15 +80,19 @@ func (p *Picomposer) generateImages() {
 	}
 
 	for i, t := range p.tuples {
-		ctx := gg.NewContextForImage(t[0].img)
-		name := "out_"
-		for _, ni := range t {
-			ctx.DrawImage(ni.img, 0, 0)
-			name = name + "_" + ni.name
+		if len(t) > 0 {
+			ctx := gg.NewContextForImage(t[0].img)
+			name := "out_"
+			for _, ni := range t {
+				ctx.DrawImage(ni.img, 0, 0)
+				name = name + "_" + ni.name
+			}
+			path := p.outputFolder + "/" + name + ".png"
+			ctx.SavePNG(path)
+			fmt.Println("Saved " + strconv.FormatInt(int64(i + 1), 10) + "/" + strconv.FormatInt(int64(len(p.tuples)), 10))
+		} else {
+			fmt.Println("Something wrong with input folder, no tuples got")
 		}
-		path := p.outputFolder + "/" + name + ".png"
-		ctx.SavePNG(path)
-		fmt.Println("Saved " + strconv.FormatInt(int64(i + 1), 10) + "/" + strconv.FormatInt(int64(len(p.tuples)), 10))
 	}
 }
 
@@ -97,7 +107,6 @@ func main() {
 		assetsFolder: *inputFolder,
 		outputFolder: *outputFolder,
 		tuples: make([][]namedImage, 0),
-		images: make(map[int][]namedImage),
 	}
 
 	p.loadImages()
